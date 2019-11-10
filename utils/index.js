b92d9abb74a41b6b1ad41a988b84d7d031e78dd6
Jonas Lundberg <jonasl@iamjonas.me>
2019-11-10 15:07:05 +0100

    Adding levenshtein completion
    Will be used by commands
    when the user (e g me) spells
    badly.
    
    Test plan:
    * Run function with a completion
      that will match (it's levenshtein
      distance is less than or equal to 3).
      Verify the completion messages is
      printed out.
    * Run function with some options that
      does not have a levenshtein distance
      less than or equal to three. Verify
      the notcompleted message is printed.

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,7 @@
+const fastLevenshtein = require('fast-levenshtein');
+
+const maxLevenshteinDistanceForCompletion = 3;
+
 module.exports = {
   /**
    * For unrecoverable errors, prints to the error
@@ -6,7 +10,28 @@ module.exports = {
   errorAndDie(message, error) {
     console.error(`Internal error:`);
     console.error(message);
-    console.error(`Error was: ${error}`)
-    process.exit(1)
+    console.error(`Error was: ${error}`);
+
+    process.exit(1);
+  },
+  didYouMean(
+    name,
+    completions,
+    completionFoundMessage,
+    completionNotFoundMessage
+  ) {
+    const foundCompletion = completions.find(
+      completion =>
+        fastLevenshtein.get(name, completion) <=
+        maxLevenshteinDistanceForCompletion
+    );
+
+    if (foundCompletion) {
+      console.error(completionFoundMessage.replace('$1', foundCompletion));
+    } else {
+      console.error(completionNotFoundMessage);
+    }
+
+    process.exit(1);
   }
-}
+};
