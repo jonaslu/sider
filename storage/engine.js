bdcdd2d72f48af8e5770089a5e429c67fd936942
Jonas Lundberg <jonasl@iamjonas.me>
2019-11-12 17:53:15 +0100

    Adding reading of engine settings
    Test plan:
    * Have no settings (no engines/ folder).
      Verify the default settings (e g port)
      is returned when calling getEngineConfig.
    * Have some valid settings on an engine.
      Verify that the default setting (port)
      is merged with the disk-setting and that
      the disk-setting has higher priority.
    * Make some error in the settings.json,
      verify an error-message is printed.

---
+++ storage/engine.js
@@ -0,0 +1,48 @@
+const fsExtra = require('fs-extra');
+const path = require('path');
+
+const engines = require('../engines');
+const settings = require('../settings');
+
+const { errorAndDie } = require('../utils');
+const { engineStoragePath } = require('../config');
+
+/**
+ * {
+ * // For the config file2
+ *  config: {
+ *  }
+ * }
+ */
+
+const settingsFileName = 'settings.json';
+
+module.exports = {
+  async getEngineConfig(engineName) {
+    const engine = await engines.getEngine(engineName);
+    const defaultSettings = engine.getConfig();
+
+    const engineConfigFile = path.join(
+      engineStoragePath,
+      engineName,
+      settingsFileName
+    );
+
+    let diskConfig = {};
+    const configExists = await fsExtra.exists(engineConfigFile);
+    if (configExists) {
+      try {
+        diskConfig = await fsExtra.readJSON(engineConfigFile, 'utf-8');
+      } catch (e) {
+        errorAndDie(
+          `Could not read engine ${engineName} setings.json ${engineConfigFile}`,
+          e
+        );
+      }
+    }
+
+    return {
+      config: settings.mergeSettings(defaultSettings, diskConfig.config)
+    };
+  }
+};
