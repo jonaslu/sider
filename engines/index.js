cf6801decce861d356e1a72bb102228aad9d1904
Jonas Lundberg <jonasl@iamjonas.me>
2019-11-19 07:47:42 +0100

    Make get engines safer
    If we can't find the engine - if
    the settings file has been tampered
    with on the db or the storage,
    bail.
    
    Adding this to getting config too,
    it's double work when running
    sider db start, but it's better
    to be safe here.
    
    Test plan:
    * Hack the engine/index and
      verify that the program exists
      with status 1 when trying to
      get a non-existing engine.

--- engines/index.js
+++ engines/index.js
@@ -1,7 +1,7 @@
 const fsExtra = require('fs-extra');
 const path = require('path');
 
-const { errorAndDie } = require('../utils');
+const { errorAndDie, printAndDie } = require('../utils');
 
 function getEngineFile(engineName) {
   return path.join(__dirname, `${engineName}.js`);
@@ -17,14 +17,53 @@ async function getEngine(engineName) {
       // eslint-disable-next-line
       return require(enginePath);
     } catch (e) {
-      errorAndDie(`Could not require file ${enginePath}
-It's probably a compile error in the file`, e);
+      errorAndDie(
+        `Could not require file ${enginePath}
+It's probably a compile error in the file`,
+        e
+      );
     }
   }
 
   return undefined;
 }
 
+async function getEngineOrDie(engineName) {
+  const engine = await getEngine(engineName);
+  if (!engine) {
+    printAndDie(`Could not find engine ${engineName},
+has the db settings file been tampered with?`);
+  }
+  return engine;
+}
+
 module.exports = {
-  getEngine
+  getEngine,
+  getEngineOrDie,
+  start(engineName, db, settings) {
+    const engine = getEngineOrDie(engineName);
+
+    // !! TODO !! Let engines validate the sent config
+
+    const { dbName, dbFileFolder } = db;
+
+    // commanderjs forwards any signals caught to the
+    // it's spawned child process - but the child
+    // process also get's the signal so we're
+    // called twice in the handler. Use this
+    // lo-fi flag to signal we've called stop
+    // already. .once() doesn't work.
+    let stopCalled = false;
+
+    // That ctrl+c magic
+    process.on('SIGINT', () => {
+      if (!stopCalled && engine.stop) {
+        engine.stop(dbName, settings);
+        stopCalled = true;
+      }
+    });
+
+    // !! TODO !! Check exit-value
+    return engine.start(dbFileFolder, dbName, settings);
+  }
 };
