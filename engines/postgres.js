aba2ccfe93a33b14f3f97fb36f487c6ac1f55b39
Jonas Lundberg <jonasl@iamjonas.me>
2019-12-14 22:42:15 +0100

    Use common docker-util with postgres
    Test plan:
    * Hijack the module.exports and start
      an existing engine. Verify it starts.

--- engines/postgres.js
+++ engines/postgres.js
@@ -1,57 +1,5 @@
 const fs = require('fs-extra');
-const os = require('os');
-const { spawn } = require('child_process');
-
-// !! TODO !! Create a docker-runner helper - unify this and redis
-// !! TODO !! Make ports and other stuff configurable
-function runDb(dbPath, dbName, runtimeConfig, echoOutput = true) {
-  let osSpecificArgs = [];
-  const { port } = runtimeConfig;
-
-  const platform = os.platform();
-  if (platform === 'linux') {
-    const { uid, gid } = os.userInfo();
-    osSpecificArgs = [
-      '-v',
-      '/etc/group:/etc/group:ro',
-      '-v',
-      '/etc/passwd:/etc/passwd:ro',
-      '-u',
-      `${uid}:${gid}`
-    ];
-  }
-
-  const args = [
-    'run',
-    '--rm',
-    ...osSpecificArgs,
-    '-v',
-    `${dbPath}:/var/lib/postgresql/data`,
-    '-p',
-    `${port}:5432`,
-    '--name',
-    dbName,
-    'postgres'
-  ];
-
-  const childProcess = spawn('docker', args);
-
-  if (echoOutput) {
-    childProcess.stdout.on('data', data =>
-      process.stdout.write(`${data.toString('utf-8')}`)
-    );
-
-    childProcess.stderr.on('data', data => {
-      process.stderr.write(`${data.toString('utf-8')}`);
-    });
-  }
-
-  return new Promise(resolve => {
-    childProcess.on('close', resolve);
-  });
-}
-
-// Start batch calls the above with printStdout = false
+const { runDb } = require('./docker-utils');
 
 module.exports = {
   // !! TODO !! Make this return a promise (or have a done callback)
@@ -74,6 +22,16 @@ module.exports = {
     };
   },
   start(dbPath, dbName, runtimeConfig) {
-    return runDb(dbPath, dbName, runtimeConfig);
+    const { port } = runtimeConfig;
+
+    const dockerArgs = [
+      '-v',
+      `${dbPath}:/var/lib/postgresql/data`,
+      '-p',
+      `${port}:5432`
+    ];
+    const dockerImageAndCommand = ['postgres'];
+
+    return runDb(dbName, dockerArgs, dockerImageAndCommand);
   }
 };
