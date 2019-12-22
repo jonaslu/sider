cfec989929d6f6713f3789c1ff218708e43d7167
Jonas Lundberg <jonasl@iamjonas.me>
2019-12-22 16:08:22 +0100

    Adding empty snaphots
    Test plan:
    * Try to create a snapshot with a name
      that already exists. Verify error-message.
    * Try to add a snapshot with an engine
      that does not exist. Verify didYouMean kicks
      in.
    * Verify any cli-config is merged with the default
      engine config and that the engine starts.
      Verify files are put into the empty snapshots folder
      and that the snapshot specs.json file contains sane values.
    * Set the persist flag and verify the runtimeCliSettings
      are persisted to the specs.json.

--- commands/sider-snapshot.js
+++ commands/sider-snapshot.js
@@ -1,12 +1,18 @@
-const engines = require('../engines');
+const chalk = require('chalk');
 const fsExtra = require('fs-extra');
+
+const engines = require('../engines');
 const snapshots = require('../storage/snapshots');
 const { printUserErrorAndDie, didYouMean } = require('../utils');
+const { getEngineRuntimeConfig } = require('../storage/engine');
+const { mergeRuntimeConfig } = require('../runtime/config');
 
 async function addSnapshot(engineName, snapshotName, dumpBasePath) {
   const dumpBasePathExists = await fsExtra.pathExists(dumpBasePath);
   if (!dumpBasePathExists) {
-    printUserErrorAndDie(`Cannot add snapshot. Path ${dumpBasePath} does not exist`);
+    printUserErrorAndDie(
+      `Cannot add snapshot. Path ${dumpBasePath} does not exist`
+    );
   }
 
   const snapshotExists = await snapshots.getSnapshot(snapshotName);
@@ -14,10 +20,74 @@ async function addSnapshot(engineName, snapshotName, dumpBasePath) {
     printUserErrorAndDie(`Snapshot ${snapshotName} already exists`);
   }
 
-  const engine  = await engines.getEngine(engineName);
+  const engine = await engines.getEngine(engineName);
   if (!engine) {
-    didYouMean(engineName, await engines.getAllEngineNames(), `Engine`)
+    didYouMean(engineName, await engines.getAllEngineNames(), `Engine`);
   }
 
-  await snapshots.createSnapshot(snapshotName, engine, engineName, dumpBasePath);
+  await snapshots.createImportSnapshot(
+    snapshotName,
+    engine,
+    engineName,
+    dumpBasePath
+  );
 }
+
+async function addEmptySnapshot(
+  engineName,
+  snapshotName,
+  cliRuntimeConfig,
+  persist
+) {
+  const snapshotExists = await snapshots.getSnapshot(snapshotName);
+  if (snapshotExists) {
+    printUserErrorAndDie(`Snapshot ${snapshotName} already exists`);
+  }
+
+  const allEngineNames = await engines.getAllEngineNames();
+  const engineExists = allEngineNames.some(
+    installedEngineName => installedEngineName === engineName
+  );
+
+  if (!engineExists) {
+    didYouMean(engineName, await engines.getAllEngineNames(), `Engine`);
+  }
+
+  const engineRuntimeConfig = await getEngineRuntimeConfig(engineName);
+
+  const snapshotRuntimeConfig = mergeRuntimeConfig(
+    engineRuntimeConfig.runtimeConfig,
+    cliRuntimeConfig
+  );
+
+  const { port } = snapshotRuntimeConfig;
+
+  console.log(
+    chalk.green(`✨ Starting empty snapshot ${snapshotName} on port ${port} 🚀`)
+  );
+  await snapshots.createEmptySnapshot(
+    snapshotName,
+    engineName,
+    snapshotRuntimeConfig
+  );
+  console.log(
+    chalk.green(
+      `Sucessfully shut down empty snapshot ${chalk.blue(snapshotName)}`
+    )
+  );
+
+  if (persist) {
+    const newlyCreatedSnapshot = await snapshots.getSnapshot(snapshotName);
+    await snapshots.saveRuntimeConfig(newlyCreatedSnapshot, cliRuntimeConfig);
+  }
+}
+
+module.exports = {
+  addSnapshot(engineName, snapshotName, importSnapshotDiskPath, options) {
+    commandFound = true;
+
+    const { empty } = options;
+    if (empty && importSnapshotDiskPath) {
+    }
+  }
+};
