54c02ea34a7c15e05056df260e40fa9b2cfae45a
Jonas Lundberg <jonasl@iamjonas.me>
2019-12-29 22:24:35 +0100

    Adding clone to sider db command
    Test plan:
    * sider db clone, sider db help clone,
      sider db clone -h and sider db clone --help
      all prints the usage screen.
    * sider db clone <name> prints error
      on missing snapshot name.
    * sider db clone <db-name> <snapshot-name>
      successfully clones a snapshot to a database.

---
+++ commands/db/clone.js
@@ -0,0 +1,62 @@
+const chalk = require('chalk');
+
+const {
+  didYouMean,
+  printUserErrorAndDie,
+  printUsageAndExit,
+  containsArguments
+} = require('../../utils');
+
+const usage = `
+Usage: sider db clone [options] <name> <snapshot>
+
+Clones a database from a snapshot
+
+Options:
+  -h, --help     output usage information
+`;
+
+const dbs = require('../../storage/db');
+const snapshots = require('../../storage/snapshots');
+
+async function clone(dbName, snapshotName) {
+  const snapshot = await snapshots.getSnapshot(snapshotName);
+  if (!snapshot) {
+    didYouMean(snapshotName, await snapshots.getAllSnapshots(), 'Snapshot');
+  }
+
+  const dbExists = await dbs.getDb(dbName);
+  if (dbExists) {
+    printUserErrorAndDie(`Database ${chalk.green(dbName)} exists`);
+  }
+
+  await dbs.createDb(dbName, snapshot);
+  console.log(
+    chalk.green(
+      `✨ Sucessfully cloned database ${dbName} from snapshot ${snapshotName} 🚀`
+    )
+  );
+}
+
+async function processArgv(argv = []) {
+  if (!argv.length) {
+    printUsageAndExit(usage);
+  }
+
+  const { hasArgument: wantHelp } = containsArguments(argv, '-h', '--help');
+  if (wantHelp) {
+    printUsageAndExit(usage);
+  }
+
+  const [dbName, snapshotName] = argv;
+
+  if (!snapshotName) {
+    printUserErrorAndDie(`Missing the name of the snapshot to clone (parameter <snapshot>)`);
+  }
+
+  return clone(dbName, snapshotName);
+}
+
+module.exports = {
+  processArgv
+}
