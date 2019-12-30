a36e7e2f3dd9adc24ad1e218d66f9546e559cab9
Jonas Lundberg <jonasl@iamjonas.me>
2019-12-30 22:58:09 +0100

    Refactor: sider db clone - same thing
    Use utils to compact file length some
    and move usage below the main function.
    
    Test plan:
    * Cursory smoke testing.

--- commands/db/clone.js
+++ commands/db/clone.js
@@ -1,14 +1,23 @@
 const chalk = require('chalk');
 
-const {
-  didYouMean,
-  printUserErrorAndDie,
-  printUsageAndExit,
-  containsArguments
-} = require('../../utils');
-
 const dbs = require('../../storage/db');
 const snapshots = require('../../storage/snapshots');
+const utils = require('../../utils');
+
+async function clone(dbName, snapshotName) {
+  const snapshot = await snapshots.getSnapshot(snapshotName);
+  if (!snapshot) {
+    utils.didYouMean(snapshotName, await snapshots.getAllSnapshots(), 'Snapshot');
+  }
+
+  const dbExists = await dbs.getDb(dbName);
+  if (dbExists) {
+    utils.printUserErrorAndDie(`Database ${chalk.green(dbName)} exists`);
+  }
+
+  await dbs.createDb(dbName, snapshot);
+  console.log(chalk.green(`✨ Sucessfully cloned database ${dbName} from snapshot ${snapshotName} 🚀`));
+}
 
 const usage = `
 Usage: sider db clone [options] <name> <snapshot>
@@ -19,39 +28,20 @@ Options:
   -h, --help     output usage information
 `;
 
-async function clone(dbName, snapshotName) {
-  const snapshot = await snapshots.getSnapshot(snapshotName);
-  if (!snapshot) {
-    didYouMean(snapshotName, await snapshots.getAllSnapshots(), 'Snapshot');
-  }
-
-  const dbExists = await dbs.getDb(dbName);
-  if (dbExists) {
-    printUserErrorAndDie(`Database ${chalk.green(dbName)} exists`);
-  }
-
-  await dbs.createDb(dbName, snapshot);
-  console.log(
-    chalk.green(
-      `✨ Sucessfully cloned database ${dbName} from snapshot ${snapshotName} 🚀`
-    )
-  );
-}
-
 async function processArgv(argv = []) {
   if (!argv.length) {
-    printUsageAndExit(usage);
+    utils.printUsageAndExit(usage);
   }
 
-  const { hasArgument: wantHelp } = containsArguments(argv, '-h', '--help');
+  const { hasArgument: wantHelp } = utils.containsArguments(argv, '-h', '--help');
   if (wantHelp) {
-    printUsageAndExit(usage);
+    utils.printUsageAndExit(usage);
   }
 
   const [dbName, snapshotName] = argv;
 
   if (!snapshotName) {
-    printUserErrorAndDie(`Missing the name of the snapshot to clone (parameter <snapshot>)`);
+    utils.printUserErrorAndDie(`Missing the name of the snapshot to clone (parameter <snapshot>)`);
   }
 
   return clone(dbName, snapshotName);
@@ -59,4 +49,4 @@ async function processArgv(argv = []) {
 
 module.exports = {
   processArgv
-}
+};
