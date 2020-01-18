3c6b433a9ba5d7e42f431ae5a755d5314ae2b44f
Jonas Lundberg <jonasl@iamjonas.me>
2020-01-18 13:23:37 +0100

    Sider snapshot - add empty as new command
    sider snapshot -e previously will now and
    henceforth known as sider snapshot empty
    instead. It's more catchy.
    
    Test plan:
    * Cursory testing of the help-methods.
    * sider snapshot redis gives error on
      missing snapshot name.
    * sider snapshot empty redis sldjfk starts
      an empty snapshot on the engines default
      port.
    * sider snapshot empty -p redis ljk gives a warning
      on persist given but no parameters.
    * sider snapshot empty redis port=7564 starts
      on that port, but does not persist the port.
    * sider snapshot empty -p redis lkjsdf port=666
      starts on port=666 and also persists it to the
      snapshot specs.json file.

---
+++ commands/snapshot/empty.js
@@ -0,0 +1,70 @@
+const chalk = require('chalk');
+
+const engines = require('../../engines');
+const runtimeConfig = require('../../runtime/config');
+const { getEngineRuntimeConfig } = require('../../storage/engine');
+const { printUserErrorAndDie, didYouMean } = require('../../utils');
+const snapshots = require('../../storage/snapshots');
+const utils = require('../../utils');
+
+async function addEmptySnapshot(engineName, snapshotName, cliRuntimeConfig, persist) {
+  const snapshotExists = await snapshots.getSnapshot(snapshotName);
+  if (snapshotExists) {
+    printUserErrorAndDie(`Snapshot ${snapshotName} already exists`);
+  }
+
+  const allEngineNames = await engines.getAllEngineNames();
+  const engineExists = allEngineNames.some(installedEngineName => installedEngineName === engineName);
+
+  if (!engineExists) {
+    didYouMean(engineName, await engines.getAllEngineNames(), `Engine`);
+  }
+
+  const engineRuntimeConfig = await getEngineRuntimeConfig(engineName);
+  const snapshotRuntimeConfig = runtimeConfig.mergeRuntimeConfig(engineRuntimeConfig.runtimeConfig, cliRuntimeConfig);
+
+  const { port } = snapshotRuntimeConfig;
+
+  console.log(chalk.green(`✨ Starting empty snapshot ${snapshotName} on port ${port} 🚀`));
+
+  const newSnapshot = await snapshots.createEmptySnapshot(snapshotName, engineName, snapshotRuntimeConfig);
+
+  console.log(chalk.green(`Successfully shut down empty snapshot ${chalk.blue(snapshotName)}`));
+
+  if (persist) {
+    await snapshots.saveRuntimeConfig(newSnapshot, cliRuntimeConfig);
+  }
+}
+
+const usage = `
+Usage: sider snapshot empty [options] <engine> <name> [parameters...]
+
+Starts and then saves an empty snapshot
+
+Options:
+  -p, --persist  Persist the parameters
+  -h, --help     output usage information
+`;
+
+async function processArgv(argv = []) {
+  utils.printUsageIfHelp(argv, usage);
+
+  const { hasArgument: persist, rest } = utils.containsArguments(argv, '-p', '--persist');
+  const [engineName, snapshotName, ...runtimeConfigKeyValues] = rest;
+
+  if (!snapshotName) {
+    utils.printUserErrorAndDie(`Missing the name of the snapshot (parameter <name>)`);
+  }
+
+  if (persist && !runtimeConfigKeyValues.length) {
+    utils.printWarning(`Persist flag set but no runtime parameters (e g -p but no port=666)`);
+  }
+
+  const cliRuntimeConfig = runtimeConfig.parseRuntimeConfigKeyValues(runtimeConfigKeyValues);
+
+  return addEmptySnapshot(engineName, snapshotName, cliRuntimeConfig, persist);
+}
+
+module.exports = {
+  processArgv
+};
