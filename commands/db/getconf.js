c096c7b6f486fd77fbb424511c98386d3458c105
Jonas Lundberg <jonasl@iamjonas.me>
2020-03-02 06:10:00 +0100

    sider db getconf
    Test plan:
    * Usual help commands work
    * sider db getconf and misspelled
      db name yields didYouMean.
    * sider db getconf with
      a database with no settings
      prints 'No config set'.
    * sider db getconf on a
      database with some settings
      prints it's settings.

---
+++ commands/db/getconf.js
@@ -0,0 +1,41 @@
+const chalk = require('chalk');
+
+const dbs = require('../../storage/db');
+const utils = require('../../utils');
+const config = require('../../runtime/config')
+
+async function getConf(dbName) {
+  const db = await dbs.getDb(dbName);
+  if (!db) {
+    utils.didYouMean(dbName, await dbs.getAllDbNames(), `Database`);
+  }
+
+  const { runtimeConfigSpec } = db;
+  if (Object.keys(runtimeConfigSpec).length === 0) {
+    // !! TODO !! Make coloring consistent
+    console.log('No config set');
+    return;
+  }
+
+  config.printRuntimeConfigValues(runtimeConfigSpec);
+}
+
+const usage = `
+Usage: sider db getconf [options] <name>
+
+Displays runtime config for a database
+
+Options:
+  -h, --help     output usage information
+`;
+
+async function processArgv(argv = []) {
+  utils.printUsageIfHelp(argv, usage);
+
+  const [dbName] = argv;
+  return getConf(dbName);
+}
+
+module.exports = {
+  processArgv
+};
