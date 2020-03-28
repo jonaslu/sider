26f6f025345451d98e775eef7a1bcc264bb00162
Jonas Lundberg <jonasl@iamjonas.me>
2020-03-28 10:53:15 +0100

    Adding sider install-completion
    And sider completion. Only
    supports bash for now, zsh
    is in the works.
    
    Test plan:
    * Have no .bashrc. Verify that an
      error-message is printed warning
      that completion cannot be added to
      .bashrc
    * Make .bashrc unwriteable by the current
      user (chmod -w .bashrc). Verify error-message
      that completion could not be installed.
    * Have a happy path and verify that
      the line (. <(sider completion) )
      is written to the .bashrc file.
    * Run sider install-completion again
      and verify an error-message saying
      completion already installed.
    * Start zsh and try to source completion
      . <(sider completion). Verify an error
      message saying only supported shell for
      now is bash.

--- sider.js
+++ sider.js
@@ -5,8 +5,55 @@ const [, , ...rest] = process.argv;
 const [subcommand] = rest;
 
 switch (subcommand) {
-  case 'install-completion':
+  case 'completion': {
+    const fsExtra = require('fs-extra');
+    const path = require('path');
+
+    const completion = fsExtra.readFileSync(path.join(__dirname, 'completion.sh'), 'utf-8');
+    console.log(completion);
+
     break;
+  }
+
+  case 'install-completion': {
+    const fsExtra = require('fs-extra');
+    const untiltdify = require('untildify');
+
+    const bashRcPath = untiltdify('~/.bashrc');
+    let fileContents;
+    try {
+      fileContents = fsExtra.readFileSync(bashRcPath, 'utf-8');
+    } catch (e) {
+      console.error(`Could not read ~/.bashrc.`);
+      console.error(`Create it first if it does not exists. Otherwise check it's read permissions.`);
+      console.error(`To manually enable completion, run the line:`);
+      console.error(`. <(sider completion)`);
+
+      process.exit(1);
+    }
+
+    const bashRcLine = `. <(sider completion)`
+    if (fileContents.includes(bashRcLine)) {
+      console.error('Completion already installed - forgot to source it?');
+      process.exit(1);
+    }
+
+    let fileSplit = fileContents.split('\n');
+    fileSplit.push(bashRcLine);
+
+    try {
+      fsExtra.writeFileSync(bashRcPath, fileSplit.join('\n'));
+    } catch (e) {
+      console.error(`Could not read ~/.bashrc.`);
+      console.error(`Create it first if it does not exists. Otherwise check it's write permissions.`);
+      console.error(`To manually enable completion, run the line:`);
+      console.error(`. <(sider completion)`);
+
+      process.exit(1);
+    }
+
+    break;
+  }
 
   case 'uninstall-completion':
     break;
