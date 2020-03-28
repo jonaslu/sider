4ac3a9304225a7057cc17a26da7881509f242b48
Jonas Lundberg <jonasl@iamjonas.me>
2020-03-28 15:18:44 +0100

    Adding uninstall completion
    Test plan:
    * Have a .bashrc with no sider completion
      installed. Verify an error-message
      is printed on that no completion
      can be found.
    * Add completion by running sider
      install-completion. Verify
      completion is removed by running
      sider uninstall-completion again.
    * No regression on the .bashrc handling
      as tested in the previous commit.

--- sider.js
+++ sider.js
@@ -4,6 +4,47 @@ const { processArgv } = require('./commands/main/usage');
 const [, , ...rest] = process.argv;
 const [subcommand] = rest;
 
+function getBashRcAndSiderLine() {
+  const fsExtra = require('fs-extra');
+  const untiltdify = require('untildify');
+
+  const bashRcPath = untiltdify('~/.bashrc');
+  let fileContents;
+  try {
+    fileContents = fsExtra.readFileSync(bashRcPath, 'utf-8');
+  } catch (e) {
+    console.error(`Could not read ~/.bashrc.`);
+    console.error(`Create it first if it does not exists. Otherwise check it's read permissions.`);
+    console.error(`To manually enable completion, run the line:`);
+    console.error(`. <(sider completion)`);
+
+    process.exit(1);
+  }
+
+  const splitBashRcLines = fileContents.split('\n');
+  const siderCompletionLine = splitBashRcLines.findIndex(line => line.match(/\s*\. <\(sider completion\)\s*$/));
+
+  return { siderCompletionLine, splitBashRcLines };
+}
+
+function writeBashRc(splitFileContents) {
+  const fsExtra = require('fs-extra');
+  const untiltdify = require('untildify');
+
+  const bashRcPath = untiltdify('~/.bashrc');
+
+  try {
+    fsExtra.writeFileSync(bashRcPath, splitFileContents.join('\n'));
+  } catch (e) {
+    console.error(`Could not read ~/.bashrc.`);
+    console.error(`Create it first if it does not exists. Otherwise check it's write permissions.`);
+    console.error(`To manually enable completion, run the line:`);
+    console.error(`. <(sider completion)`);
+
+    process.exit(1);
+  }
+}
+
 switch (subcommand) {
   case 'completion': {
     const fsExtra = require('fs-extra');
@@ -16,46 +57,31 @@ switch (subcommand) {
   }
 
   case 'install-completion': {
-    const fsExtra = require('fs-extra');
-    const untiltdify = require('untildify');
+    const { siderCompletionLine, splitBashRcLines } = getBashRcAndSiderLine();
 
-    const bashRcPath = untiltdify('~/.bashrc');
-    let fileContents;
-    try {
-      fileContents = fsExtra.readFileSync(bashRcPath, 'utf-8');
-    } catch (e) {
-      console.error(`Could not read ~/.bashrc.`);
-      console.error(`Create it first if it does not exists. Otherwise check it's read permissions.`);
-      console.error(`To manually enable completion, run the line:`);
-      console.error(`. <(sider completion)`);
-
-      process.exit(1);
-    }
-
-    const bashRcLine = `. <(sider completion)`
-    if (fileContents.includes(bashRcLine)) {
+    if (siderCompletionLine !== -1) {
       console.error('Completion already installed - forgot to source it?');
       process.exit(1);
     }
 
-    let fileSplit = fileContents.split('\n');
-    fileSplit.push(bashRcLine);
+    const bashRcLine = `. <(sider completion)`;
+    splitBashRcLines.push(bashRcLine);
 
-    try {
-      fsExtra.writeFileSync(bashRcPath, fileSplit.join('\n'));
-    } catch (e) {
-      console.error(`Could not read ~/.bashrc.`);
-      console.error(`Create it first if it does not exists. Otherwise check it's write permissions.`);
-      console.error(`To manually enable completion, run the line:`);
-      console.error(`. <(sider completion)`);
-
-      process.exit(1);
-    }
-
+    writeBashRc(splitBashRcLines);
     break;
   }
 
   case 'uninstall-completion':
+    const { siderCompletionLine, splitBashRcLines } = getBashRcAndSiderLine();
+
+    if (siderCompletionLine === -1) {
+      console.error('Completion not installed - cannot remove it.');
+      process.exit(1);
+    }
+
+    splitBashRcLines.splice(siderCompletionLine, 1);
+
+    writeBashRc(splitBashRcLines);
     break;
 
   default:
