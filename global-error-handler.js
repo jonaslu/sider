dee0bf22b645fd15961ee1fa9d1d12a7a793f3df
Jonas Lundberg <jonasl@iamjonas.me>
2020-04-04 13:26:08 +0200

    Nuke everything in preparation for v1.0
    It's been living on a branch for very long, now
    it's time for v1.0 to enter.
    
    Almost a complete rewrite. Enjoy.

--- global-error-handler.js
+++
@@ -1,7 +0,0 @@
-function printErrorAndExit(error) {
-  console.error(error);
-  process.exit(1);
-}
-
-process.on('unhandledRejection', error => printErrorAndExit(error));
-process.on('uncaughtException', error => printErrorAndExit(error));
--- helpers.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  ensureFolder(folder) {
-    return folder.endsWith(path.sep) ? folder : path.join(folder, path.sep);
-  }
-};
