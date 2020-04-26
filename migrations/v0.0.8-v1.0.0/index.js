e0beed78ce842d8b254fab52dae93552dd981e01
Jonas Lundberg <jonasl@iamjonas.me>
2020-04-26 13:54:53 +0200

    Migration: make script standalone
    Also, make it wait for any keypress.
    
    Test plan:
    * Run script standalone and verify
      it waits before migrating.

--- migrations/v0.0.8-v1.0.0/index.js
+++ migrations/v0.0.8-v1.0.0/index.js
@@ -2,18 +2,19 @@ const { migrateAllEngines } = require('./engine');
 const { migrateAllSnapshots } = require('./snapshots');
 const { migrateAllDbs } = require('./dbs');
 
-function migrate() {
-  const { baseDir, engineStoragePath, dbsStoragePath, snapshotsStoragePath } = require('./v0_0_8_siderrc');
+const { baseDir, engineStoragePath, dbsStoragePath, snapshotsStoragePath } = require('./v0_0_8_siderrc');
 
-  // Have a notification on backing up, or
-  // committing everything to git first to be
-  // on the safe side.
-  console.log(`About to start migration of contents in path ${baseDir}`);
-  console.log(`Just to be on the super-safe side of things:`);
-  console.log(`You're adivsed to back up the contents in this folder,`);
-  console.log(`or run git init && git add . && git commit -m "backup"`);
-  console.log(`in that folder just in case.`);
-  console.log(`\nPress any key once that's done and we'll continue`);
+console.log(`About to start migration of contents in path ${baseDir}`);
+console.log(`Just to be on the super-safe side of things:`);
+console.log(`You're adivsed to back up the contents in this folder,`);
+console.log(`or run git init && git add . && git commit -m "backup"`);
+console.log(`in that folder just in case.`);
+
+process.stdout.write(`\nPress any key once that's done and we'll continue > `);
+process.stdin.setRawMode(true);
+
+process.stdin.on('data', () => {
+  process.stdin.resume();
 
   console.log(`\nMigrating any engines in folder ${engineStoragePath}`);
   migrateAllEngines();
@@ -26,9 +27,7 @@ function migrate() {
 
   console.log(`\n\nDone! If there were errors - act on them.`);
   console.log(`If not, give the new v1.0.0 a test-whirl and`);
-  console.log(`then you can delete the backup.`)
-}
+  console.log(`then you can delete the backup.`);
 
-module.exports = {
-  migrate,
-};
+  process.exit(0);
+});
