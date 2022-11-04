4568dbcf3e35b3f02f15c33e563b7209db4f9f21
Jonas Lundberg <jonasl@iamjonas.me>
2022-11-04 18:40:58 +0100

    Migration: Copy the v1.0.0 siderrc to migration
    We're only going to need the snapshots
    base path, the rest can be removed.
    
    Test plan:
    * Quick smoke test that the .siderrc
      file is read if exists and that the
      snapshots folder is correctly assembled.

---
+++ migrations/v1.1.0-v1.2.0/v1_0_0_siderrc.js
@@ -0,0 +1,33 @@
+const fsExtra = require('fs-extra');
+const path = require('path');
+const untildify = require('untildify');
+
+const { internalErrorAndDie } = require('../../utils');
+
+function ensureFolder(folder) {
+  return folder.endsWith(path.sep) ? folder : path.join(folder, path.sep);
+}
+
+const siderRcPath = untildify('~/.siderrc');
+let siderRcSettings = { basePath: '~/.sider' };
+
+try {
+  siderRcSettings = {
+    ...siderRcSettings,
+    ...fsExtra.readJSONSync(siderRcPath),
+  };
+} catch (e) {
+  if (e.code !== 'ENOENT') {
+    internalErrorAndDie(`Could not read rcfile ${siderRcPath}`, e);
+  }
+}
+
+const siderBaseDir = untildify(siderRcSettings.basePath);
+const baseDir = ensureFolder(siderBaseDir);
+
+const snapshotsStoragePath = `${baseDir}snapshots/`;
+
+module.exports = {
+  snapshotsStoragePath,
+};
+
