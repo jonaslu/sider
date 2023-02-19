19bc4c7ebaa62ae98a681291c737020c0af0e729
Jonas Lundberg <jonasl@iamjonas.me>
2023-02-19 13:23:50 +0100

    V1.1.0 - V1.2.0 migration: remove readline questions
    Same deal as with v0.0.8-v1.0.0, don't
    ask questions, simply do the migration.
    
    Test plan:
    * Have a sider v1.1.0 installation with some
      snapshots created and setconf run on them
      Run npm i -g --foreground-scripts .
      and verify it passes (no regression).

--- migrations/v1.1.0-v1.2.0/index.js
+++ migrations/v1.1.0-v1.2.0/index.js
@@ -1,22 +1,15 @@
+const { detectMigrationToV1_2_0 } = require('./check');
 const { patchSnapshots } = require('./patch');
-const { baseDir, snapshotsStoragePath } = require('./v1_0_0_siderrc');
 
-console.log(`About to start migration of contents in path ${snapshotsStoragePath}.\n`);
-
-console.log(`You're advised to back up the contents in the ${baseDir} folder,`);
-console.log(`Example via git: cd ${baseDir} && git init && git add . && git commit -m "V1.2.0 migration backup"`);
+const { snapshotsStoragePath } = require('./v1_0_0_siderrc');
 
 module.exports = {
-  async main(readline) {
-    await new Promise(resolve => {
-      readline.question('Press any key to continue > ', resolve);
-    });
+  migrateToV1_2_0() {
+    if (detectMigrationToV1_2_0()) {
+      console.log(`About to start migration of contents in path ${snapshotsStoragePath}.\n`);
 
-    console.log(`\nMigrating any snapshots in folder ${snapshotsStoragePath}`);
-    patchSnapshots();
-
-    console.log(`\nDone! If there were errors - act on them.`);
-    console.log(`If not, give the new version a test-whirl and`);
-    console.log(`then you can delete the backup.`);
-  },
+      console.log(`\nMigrating any snapshots in folder ${snapshotsStoragePath}`);
+      patchSnapshots();
+    }
+  }
 };
